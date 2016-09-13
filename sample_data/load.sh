14123ba84519de8017241ee54e4f8eadf0be9ef2
nreese <reese.nathan@gmail.com>
2016-09-13 16:40:27 -0600

    adding sample data

---
+++ sample_data/load.sh
@@ -0,0 +1,14 @@
+echo -e "\nRemove any cruft, delete and reload indexes"
+../mappings/load_mappings.sh
+
+echo -e "\nLoading region sample data"
+curl -XPOST 'localhost:9200/murmuration_region/region/_bulk?pretty' --data-binary "@regions.json"
+
+echo -e "\nLoading target sample data"
+curl -XPOST 'localhost:9200/murmuration_target/target/_bulk?pretty' --data-binary "@targets.json"
+
+echo -e "\nLoading objective sample data"
+curl -XPOST 'localhost:9200/murmuration_objective/objective/_bulk?pretty' --data-binary "@objectives.json"
+
+echo -e "\nLoading resource sample data"
+curl -XPOST 'localhost:9200/murmuration_resource/resource/_bulk?pretty' --data-binary "@resources.json"
