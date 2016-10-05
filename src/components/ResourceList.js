0032e17d5b8830aae3328349a2d07d03891d87d6
skarp <sam.d.karp@gmail.com>
2016-10-04 23:39:42 -0600

    Added components to view to display objective and pass data through children views

---
+++ src/components/ResourceList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ResourceList = React.createClass({
+	render: function() {
+		return (
+			<div> Resource List goes here</div>
+		)
+	}
+})
+export default ResourceList;
