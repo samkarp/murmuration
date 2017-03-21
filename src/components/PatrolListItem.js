344da76d9604d4b123335d2f8b0621a7cee5fb91
BenSlaughter <allsportster@gmail.com>
2017-03-20 19:15:51 -0600

    Adding functionality for Patrol page. Not really working state.

--- src/components/PatrolListItem.js
+++ src/components/PatrolListItem.js
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react';
-import ItemTypes from './ItemTypes';
+import { findDOMNode } from 'react-dom';
 import { DragSource, DropTarget } from 'react-dnd';
+import flow from 'lodash/flow';
+import ItemTypes from './ItemTypes';
 
 const style = {
   border: '1px dashed gray',
@@ -14,64 +16,73 @@ const cardSource = {
   beginDrag(props) {
     return {
       id: props.id,
-      originalIndex: props.findCard(props.id).index
+      index: props.index
     };
-  },
-
-  endDrag(props, monitor) {
-    const { id: droppedId, originalIndex } = monitor.getItem();
-    const didDrop = monitor.didDrop();
-
-    if (!didDrop) {
-      props.moveCard(droppedId, originalIndex);
-    }
   }
 };
 
 const cardTarget = {
-  canDrop() {
-    return false;
-  },
+  hover(props, monitor, component) {
+    const dragIndex = monitor.getItem().index;
+    const hoverIndex = props.index;
 
-  hover(props, monitor) {
-    const { id: draggedId } = monitor.getItem();
-    const { id: overId } = props;
-
-    if (draggedId !== overId) {
-      const { index: overIndex } = props.findCard(overId);
-      props.moveCard(draggedId, overIndex);
+    // Don't replace items with themselves
+    if (dragIndex === hoverIndex) {
+      return;
     }
+
+    // Determine rectangle on screen
+    const hoverBoundingRect = findDOMNode(component).getBoundingClientRect();
+
+    // Get vertical middle
+    const hoverMiddleY = (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+
+    // Determine mouse position
+    const clientOffset = monitor.getClientOffset();
+
+    // Get pixels to the top
+    const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+
+    // Only perform the move when the mouse has crossed half of the items height
+    // When dragging downwards, only move when the cursor is below 50%
+    // When dragging upwards, only move when the cursor is above 50%
+
+    // Dragging downwards
+    if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+      return;
+    }
+
+    // Dragging upwards
+    if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+      return;
+    }
+
+    // Time to actually perform the action
+    props.moveCard(dragIndex, hoverIndex);
+
+    // Note: we're mutating the monitor item here!
+    // Generally it's better to avoid mutations,
+    // but it's good here for the sake of performance
+    // to avoid expensive index searches.
+    monitor.getItem().index = hoverIndex;
   }
 };
 
-function collect(connect, monitor) {
-  console.log( "HERE2", connect );
-  return {
-    connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver(),
-    canDrop: monitor.canDrop()
-  };
-}
-
-function collect2(connect, monitor) {
-  return {
-    connectDragSource: connect.dragSource(),
-    connectDragPreview: connect.dragPreview(),
-    isDragging: monitor.isDragging()
-  };
-}
-
 class PatrolListItem extends React.Component {
-
+ 
   render() {
-
     const { text, isDragging, connectDragSource, connectDropTarget } = this.props;
     const opacity = isDragging ? 0 : 1;
 
+    const newStyle = Object.assign({},style);
+
+    newStyle.opactiy = opacity;
+    console.log(newStyle);
+
     return connectDragSource(connectDropTarget(
-      <div >
+      <div style={newStyle}>
         {text}
-      </div>
+      </div>,
     ));
   }
 }
@@ -79,12 +90,20 @@ class PatrolListItem extends React.Component {
 PatrolListItem.propTypes = {
   connectDragSource: PropTypes.func.isRequired,
   connectDropTarget: PropTypes.func.isRequired,
+  index: PropTypes.number.isRequired,
   isDragging: PropTypes.bool.isRequired,
   id: PropTypes.any.isRequired,
   text: PropTypes.string.isRequired,
   moveCard: PropTypes.func.isRequired,
-  findCard: PropTypes.func.isRequired
 };
 
-const x = DropTarget(ItemTypes.CARD, cardTarget, collect )(PatrolListItem);
-export default DragSource(ItemTypes.CARD, cardSource, collect2 )( x )
+
+export default flow(
+  DropTarget(ItemTypes.CARD, cardTarget, connect => ({
+  connectDropTarget: connect.dropTarget()
+})),
+DragSource(ItemTypes.CARD, cardSource, (connect, monitor) => ({
+  connectDragSource: connect.dragSource(),
+  isDragging: monitor.isDragging()
+}))
+)(PatrolListItem)
